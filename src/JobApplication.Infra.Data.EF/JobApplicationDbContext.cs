2be0642fbca87e9f79ffc399756a78d9d74aa642
Anthony Coutinho <thonycout@gmail.com>
2024-01-30 12:53:58 -0300

    adding to onConfiguring the connection string

--- src/JobApplication.Infra.Data.EF/JobApplicationDbContext.cs
+++ src/JobApplication.Infra.Data.EF/JobApplicationDbContext.cs
@@ -7,12 +7,19 @@ namespace JobApplication.Infra.Data.EF
     {
         public JobApplicationDbContext(DbContextOptions<JobApplicationDbContext> options) : base(options)
         {
-
+           
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new JobApplicationConfiguration());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connString = "Host=db;Username=postgres;Password=postgres;Database=JobApplicationTracker";
+
+            optionsBuilder.UseNpgsql(connString);
         }
 
     }
